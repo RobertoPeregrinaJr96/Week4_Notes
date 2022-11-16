dc3f1269e974e44b72a40bbefef0a5e74cbd0584
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-15 16:45:50 -0800

    commit

--- practice/classes.js
+++ practice/classes.js
@@ -1,17 +1,17 @@
-// by using the *class* keyword I can initalize a new class object
+//(class) by using the *class* keyword I can initalize a new class object
 class singer {
-    // the *constructor* keyword allows me to utilize the *new* keyword to creat new class instance that will inherent everything in the constructor curly braces
+    //(constructor) the *constructor* keyword allows me to utilize the *new* keyword to creat new class instance that will inherent everything in the constructor curly braces
     constructor(name, age, hitSong, band) {
-        // the this keyword allows the us to use the object properties
+        //(this) the this keyword allows the us to use the object properties
         this.name = name,
             this.age = age,
             this.hitSong = hitSong,
             this.band = band
     }
-    // having a function within the class in the *class* curly braces makes this into a instance method
+    //(instance Method) having a function within the class in the *class* curly braces makes this into a instance method
     info(obj) {
         return `${obj.name}, who is ${obj.age} years old is the lead singer of the band ${obj.band}. Currently ${obj.band} has the hit single ${obj.hitSong} making waves on the internet`
     }
 }
-
+//(module.exports) we use module.exports to send our code to another file/module
 module.exports = { singer };
--- practice/index.js
+++ practice/index.js
@@ -1,7 +1,7 @@
-// destructer the object to its name,  *require* imports the information form the path in the parentheses
+//(require) destructer the object to its name,  *require* imports the information form the path in the parentheses
 const { singer } = require(`./classes`)
 
-// we are assigning a new child class *kimbra_Lee* that will inherent the properties of the parent class *singer*
+//(new) we are assigning a new child class *kimbra_Lee* that will inherent the properties of the parent class *singer*
 const kimbra_Lee = new singer("Kimbra Lee Johnson", 32, "Hi Def Distance Romance (Reimagined)", "Kimbra")
 //console.log(kimbra)
 
@@ -9,6 +9,24 @@ const kimbra_Lee = new singer("Kimbra Lee Johnson", 32, "Hi Def Distance Romance
 // kimbra is the class object which has the instance method *info* which takes in the object *kimbra*
 console.log(kimbra_Lee.info(kimbra_Lee))
 //-----------------------------------------------------------------------------
-const Amelia_Murray = new singer("Amelia Murray", 29, "Break!", "Fazerdaze")
-console.log(Amelia_Murray.info(Amelia_Murray))
+// (extends) we can use *extends* to make our class inheret properties from its parent class
+class Amelia_Murray extends singer {
+    constructor(name, hitSong, band, placeOfBirth, located) {
+        //(super) the *super* keyword allows to grab the properties i want to use from the parent consturctor
+        super(name, hitSong, band)
+        this.placeOfBirth = placeOfBirth
+        this.located = located
+    }
+    // (polymorphism)  we can inheret the functions from the parent and reassign them however we want
+    info() {
+        return `${this.name} was born in ${this.placeOfBirth},however later moved to ${this.located} `
+    }
+}
+const Amelia = new Amelia_Murray(
+    "Amelia Murray",
+    "Break!",
+    "Fazerdaze",
+    "Wellington New Zealand",
+    "Auckland New Zealand")
+console.log(Amelia.info())
 //console.log(Amelia_Murray)
