ffa95732028baeb17b216b2553b2fcc9226fa128
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-15 20:35:58 -0800

    Update index.js

--- practice/index.js
+++ practice/index.js
@@ -7,13 +7,14 @@ const kimbra_Lee = new singer("Kimbra Lee Johnson", 32, "Hi Def Distance Romance
 
 
 // kimbra is the class object which has the instance method *info* which takes in the object *kimbra*
-console.log(kimbra_Lee.info(kimbra_Lee))
+// console.log(kimbra_Lee.info(kimbra_Lee))
 //-----------------------------------------------------------------------------
 // (extends) we can use *extends* to make our class inheret properties from its parent class
 class Amelia_Murray extends singer {
     constructor(name, hitSong, band, placeOfBirth, located) {
         //(super) the *super* keyword allows to grab the properties i want to use from the parent consturctor
         super(name, hitSong, band)
+        this.band = band
         this.placeOfBirth = placeOfBirth
         this.located = located
     }
@@ -28,5 +29,40 @@ const Amelia = new Amelia_Murray(
     "Fazerdaze",
     "Wellington New Zealand",
     "Auckland New Zealand")
-console.log(Amelia.info())
+// console.log(Amelia.info())
 //console.log(Amelia_Murray)
+//console.log(Amelia.band)
+
+//-----------------------------------------------------------------------------
+
+class SecretePreforance extends singer {
+    constructor(hitSong, band, from, arePlaying, hasMerch) {
+        super(hitSong, band)
+        this.band = band
+        this.from = from
+        this.arePlaying = arePlaying
+        this.hasMerch = hasMerch
+    }
+
+    sellingMerch() {
+        if (this.merch >= 1 || this.arePlaying === true) {
+            this.merch -= 1
+            this.arePlaying = false
+            return `${this.band} are preforming and selling merch there`
+        } else {
+            return `${this.band} are not preforming and ran out of merch at their last show sorry`
+        }
+    }
+
+}
+
+const Kalush = new SecretePreforance(
+    "Stefania",
+    "Kalush Orchestra",
+    "Ukraine",
+    true,
+    1
+)
+
+console.log(Kalush.sellingMerch())
+console.log(Kalush.sellingMerch())
